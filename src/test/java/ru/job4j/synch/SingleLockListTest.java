e451b4e6a9bde9faea121754e982026ecc1a3732
Igor Kioresko <ikioresko@gmail.com>
2021-08-03 16:15:53 +0500

    4. ThreadSafe динамический список [#1105]

---
+++ src/test/java/ru/job4j/synch/SingleLockListTest.java
@@ -0,0 +1,57 @@
+package ru.job4j.synch;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class SingleLockListTest {
+    @Test
+    public void add() throws InterruptedException {
+        SingleLockList<Integer> list = new SingleLockList<>(new ArrayList<>());
+        Thread first = new Thread(() -> list.add(1));
+        Thread second = new Thread(() -> list.add(2));
+        first.start();
+        second.start();
+        first.join();
+        second.join();
+        Set<Integer> rsl = new TreeSet<>();
+        list.iterator().forEachRemaining(rsl::add);
+        assertThat(rsl, is(Set.of(1, 2)));
+    }
+
+    @Test
+    public void copyTestWhenAddValueAfterCreatingIterator() throws InterruptedException {
+        SingleLockList<Integer> list = new SingleLockList<>(new ArrayList<>());
+        Thread first = new Thread(() -> list.add(1));
+        Thread second = new Thread(() -> list.add(2));
+        first.start();
+        second.start();
+        first.join();
+        second.join();
+        Set<Integer> rsl = new TreeSet<>();
+        Iterator<Integer> it = list.iterator();
+        list.add(3);
+        it.forEachRemaining(rsl::add);
+        assertThat(rsl, is(Set.of(1, 2)));
+    }
+
+    @Test
+    public void get() throws InterruptedException {
+        SingleLockList<Integer> list = new SingleLockList<>(new ArrayList<>());
+        Thread first = new Thread(() -> list.add(1));
+        Thread second = new Thread(() -> list.add(2));
+        first.start();
+        second.start();
+        first.join();
+        second.join();
+        Set<Integer> rsl = new TreeSet<>();
+        list.iterator().forEachRemaining(rsl::add);
+        assertThat(rsl, is(Set.of(1, 2)));
+    }
+}
