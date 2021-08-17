18494f9d5187d24b50869f01304f781eaee51b72
Igor Kioresko <ikioresko@gmail.com>
2021-08-17 21:06:39 +0500

    2. ExecutorService рассылка почты. [#63097 #190863]

--- src/main/java/ru/job4j/mail/EmailNotification.java
+++ src/main/java/ru/job4j/mail/EmailNotification.java
@@ -4,7 +4,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class EmailNotification {
-    ExecutorService pool = Executors.newFixedThreadPool(
+    private final ExecutorService pool = Executors.newFixedThreadPool(
             Runtime.getRuntime().availableProcessors());
 
     public void emailTo(User user) {
@@ -17,6 +17,13 @@ public class EmailNotification {
 
     public void close() {
         pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void send(String subject, String body, String email) {
